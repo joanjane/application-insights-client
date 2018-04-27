602834be559c330f475514c3e542eddd29951d12
Joan Jané <jjane@plainconcepts.com>
2018-04-27 09:35:20 +0200

    Updates

--- src/Services/Mock/HttpMock.js
+++
@@ -1,7 +0,0 @@
-import mock from './BasicTraceMock.json';
-
-const HttpMock = {
-    'https://api.applicationinsights.io/v1/apps/BasicTraceMock/query?query=traces%20%7C%20sort%20by%20timestamp%20desc%20%7C%20limit%2050&timespan=P7D': mock
-};
-
-export default HttpMock;
