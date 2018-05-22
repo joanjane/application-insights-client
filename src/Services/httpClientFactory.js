431fc32834b7bbbd412714627b53748fd7200523
Joan Jané <jjaneballester@gmail.com>
2018-05-22 23:30:21 +0200

    Added capability to run app in demo mode with mocked apps BasicTraceMock and BasicTraceMock2

--- src/Services/httpClientFactory.js
+++ src/Services/httpClientFactory.js
@@ -2,7 +2,7 @@ import MockHttpClient from './Mock/MockHttpClient';
 import HttpClient from './HttpClient';
 
 export default function httpClientFactory() {
-    if (process.env.NODE_ENV === 'test') {
+    if (process.env.NODE_ENV === 'test' || process.env.REACT_APP_MODE === 'demo') {
         return new MockHttpClient();
     } else {
         return new HttpClient();
