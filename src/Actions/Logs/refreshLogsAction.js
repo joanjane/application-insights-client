27151aa0ac932778fb6e9432a20994775cefc225
Joan Jané <jjaneballester@gmail.com>
2018-06-08 20:51:31 +0200

    Fixed render test

--- src/Actions/Logs/refreshLogsAction.js
+++ src/Actions/Logs/refreshLogsAction.js
@@ -1,5 +1,5 @@
+import { GET_LOGS } from './getLogsAction';
 export const AUTOREFRESH_GET_LOGS_SOURCE = 'AUTOREFRESH';
-export const GET_LOGS = 'GET_LOGS';
 
 export function refreshLogsAction() {
     return { type: GET_LOGS, payload: { source: AUTOREFRESH_GET_LOGS_SOURCE } };
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './Store';
+import './index.css';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
+  unmountComponentAtNode(div);
 });
